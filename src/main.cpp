f8609f2fae2fd52aed59249debafa38bc84df71a
9dl <80607793+9dl@users.noreply.github.com>
2024-12-12 23:28:46 +0100

    Add delay before boot screen and restart after formatting SPIFFS in SettingsMenu

--- src/main.cpp
+++ src/main.cpp
@@ -21,6 +21,8 @@ void setup()
         M5.Power.powerOff();
     }
 
+    delay(3500);
+
     boot::BootScreen();
 }
 
--- src/modules/menus/SettingsMenu.cpp
+++ src/modules/menus/SettingsMenu.cpp
@@ -8,7 +8,7 @@ void SettingsMenu::ShowMenu() {
         {"Deep Sleep", [=] { Settings::DeepSleep(); }},
         {"Light Sleep", [=] { Settings::LightSpeed(); }},
         {"Reboot", [=] { Settings::Reboot(); }},
-        {"Format SPIFFS", [] { files::format(); }},
+        {"Format SPIFFS", [] { files::format(); ESP.restart(); }},
         {"Main Menu", [] { menu::MainMenu(); }},
     };
     print::LoopOptions(options, "WiFi Menu", 0);
